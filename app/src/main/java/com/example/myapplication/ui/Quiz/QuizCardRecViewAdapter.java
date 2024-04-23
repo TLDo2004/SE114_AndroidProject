8ce9d8f32d64a0b69972bb02767cfda9bc272907
22520743@gm.uit.edu.vn <22520743@gm.uit.edu.vn>
2024-04-23 17:58:25 +0700

    quiz UI

---
+++ app/src/main/java/com/example/myapplication/ui/Quiz/QuizCardRecViewAdapter.java
@@ -0,0 +1,71 @@
+package com.example.myapplication.ui.Quiz;
+
+import android.annotation.SuppressLint;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.myapplication.R;
+import com.example.myapplication.remote.model.QuizModel;
+import com.example.myapplication.remote.model.QuizModel;
+import com.example.myapplication.ui.Quiz.QuizCardRecViewAdapter;
+
+import android.content.Context;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QuizCardRecViewAdapter extends RecyclerView.Adapter<QuizCardRecViewAdapter.QuizCardViewHolder> {
+
+    private Context context;
+    private List<QuizModel> quizs = new ArrayList<>();
+    public QuizCardRecViewAdapter(Context context){this.context = context;};
+
+    @NonNull
+    @Override
+    public QuizCardViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.quiz_card_fragment, parent,false);
+        return new QuizCardViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull QuizCardRecViewAdapter.QuizCardViewHolder holder, int position) {
+        QuizModel item =  quizs.get(position);
+        holder.type.setText(item.type);
+//      holder.point.setText(item.point);
+        holder.card.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    @SuppressLint("NotifyDataSetChanged")
+    public void setQuizs(List<QuizModel> quizs) {
+        this.quizs = quizs;
+        notifyDataSetChanged();
+    }
+    @Override
+    public int getItemCount() {
+        return quizs.size();
+    }
+
+    public static class QuizCardViewHolder extends RecyclerView.ViewHolder {
+        private RelativeLayout card;
+        private TextView type, point, timer;
+        public QuizCardViewHolder(@Nullable View itemView) {
+            super(itemView);
+            card = itemView.findViewById(R.id.card_quiz);
+            type = itemView.findViewById(R.id.txt_type);
+            point = itemView.findViewById(R.id.txt_point);
+            timer = itemView.findViewById(R.id.txt_timer);
+        }
+    }
+}
--- app/src/main/java/com/example/myapplication/ui/Quiz/QuizFragment.java
+++ app/src/main/java/com/example/myapplication/ui/Quiz/QuizFragment.java
@@ -7,28 +7,47 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.myapplication.databinding.QuizFragmentBinding;
+
+import com.example.myapplication.remote.model.QuizModel;
+
 import com.example.myapplication.remote.repo.QuizRepo;
+
+import java.util.List;
 
 public class QuizFragment extends Fragment {
     private QuizFragmentBinding binding;
     private QuizRepo quizRepo;
+    private QuizCardRecViewAdapter quizCardAdapter;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = QuizFragmentBinding.inflate(inflater, container, false);
         initiate(binding);
-
-
+        setupRecyclerView(binding);
+        quizRepo.getQuiz().observe(getViewLifecycleOwner(), new Observer<List<QuizModel>>() {
+            @Override
+            public void onChanged(List<QuizModel> quizs) {
+                quizCardAdapter.setQuizs(quizs);
+            }
+        });
 
         return binding.getRoot();
     }
     private void initiate(QuizFragmentBinding binding) {
         quizRepo = new QuizRepo();
-
-
+        quizCardAdapter = new QuizCardRecViewAdapter(getContext());
     }
+    private void setupRecyclerView(QuizFragmentBinding binding) {
+        RecyclerView rec = binding.quizRecView;
+        rec.setLayoutManager(new LinearLayoutManager(getContext(),RecyclerView.VERTICAL,false));
+        rec.setAdapter(quizCardAdapter);
 
+        rec.setHasFixedSize(true);
+    }
     @Override
     public void onDestroyView() {
         super.onDestroyView();
