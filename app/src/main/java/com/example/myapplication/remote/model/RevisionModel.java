977eee6927f53bb21834404e9b441004f04122e4
TLDo <22520751@gm.uit.edu.vn>
2024-05-17 10:27:02 +0700

    add revisionRecAdapter

--- app/src/main/java/com/example/myapplication/remote/model/RevisionModel.java
+++ app/src/main/java/com/example/myapplication/remote/model/RevisionModel.java
@@ -3,14 +3,17 @@ package com.example.myapplication.remote.model;
 import com.google.gson.annotations.SerializedName;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class RevisionModel {
     @SerializedName("_id")
     public String id;
     @SerializedName("cre_dt")
-    public LocalDateTime cre_dt;
+    public String cre_dt;
     @SerializedName("alarm_dt")
-    public LocalDateTime alarm_dt;
+    public String alarm_dt;
     @SerializedName("interval")
     public int interval;
+//    @SerializedName("words")
+//    public List<WordModel> wordList;
 }
