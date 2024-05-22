12b5e735cae06da93ac7b22aa3d73e5afb727418
TLDo <22520751@gm.uit.edu.vn>
2024-05-22 07:15:52 +0700

    fix ip

--- app/src/main/java/com/example/myapplication/remote/client/ApiClient.java
+++ app/src/main/java/com/example/myapplication/remote/client/ApiClient.java
@@ -83,7 +83,7 @@ public class ApiClient {
         OkHttpClient client = getUnsafeOkHttpClient();
 
         retrofit = new Retrofit.Builder()
-                .baseUrl("http://192.168.2.21:3000/")
+                .baseUrl("http://10.45.15.57:3000/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .client(client)
                 .build();
